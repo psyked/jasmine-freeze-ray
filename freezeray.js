0f76e0693d5a3e59f7293b2630362a2a69a00de5
James Ford <james.f@mmtdigital.co.uk>
2013-04-03 16:50:17 +0100

    Better output and filtering when dealing with arrays

--- freezeray.js
+++ freezeray.js
@@ -2,15 +2,29 @@ function freeze(obj, prefix){
 	if(obj){
 		for(var key in obj){
 			if (obj.hasOwnProperty(key)) {
+				var stringRep;
+				if($.isArray(obj)){
+					stringRep = prefix + '[' + key +']';
+				} else {
+					stringRep = prefix + key;
+				}
 				if(key.indexOf("jQuery") === -1){
-					console.log('expects(typeof(' + prefix + key + ')).toBe("' + typeof(obj[key]) + '")');
+					if($.isArray(obj)){
+						console.log('expects(typeof(' + stringRep + ')).toBe("' + typeof(obj[key]) + '")');
+					} else {
+						console.log('expects(typeof(' + stringRep + ')).toBe("' + typeof(obj[key]) + '")');
+					}
 					if(obj[key] && !obj[key].jquery){
 						if(typeof(obj[key]) === "object"){
-							freeze(obj[key], prefix + key + '.');
+							if($.isArray(obj[key])){
+								freeze(obj[key], stringRep);
+							} else {
+								freeze(obj[key], stringRep + '.');
+							}
 						} else if(typeof(obj[key]) === "string"){
-							console.log('expects(' + prefix + key + ').toBe("' + obj[key] + '")');
+							console.log('expects(' + stringRep + ').toBe("' + obj[key] + '")');
 						} else if(typeof(obj[key]) === "number"){
-							console.log('expects(' + prefix + key + ').toBe(' + obj[key] + ')');
+							console.log('expects(' + stringRep + ').toBe(' + obj[key] + ')');
 						}
 					}
 				}
