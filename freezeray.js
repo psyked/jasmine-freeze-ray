98729a30418d3edff7a692637cdf9ed69cb55e0f
James Ford <james.f@mmtdigital.co.uk>
2013-04-03 16:14:43 +0100

    Adding basic value checks for numbers and strings.

--- freezeray.js
+++ freezeray.js
@@ -2,10 +2,17 @@ function freeze(obj, prefix){
 	if(obj){
 		for(var key in obj){
 			if (obj.hasOwnProperty(key)) {
-				console.log('expects(typeof(' + prefix + key + ')).toBe("' + typeof(obj[key]) + '")');
-				if(obj[key] && !obj[key].jquery && typeof(obj[key]) === "object"){
-					//console.log('expects(typeof(' + key + ')).toBe("object")');
-					freeze(obj[key], prefix + key + '.');
+				if(key.indexOf("jQuery") === -1){
+					console.log('expects(typeof(' + prefix + key + ')).toBe("' + typeof(obj[key]) + '")');
+					if(obj[key] && !obj[key].jquery){
+						if(typeof(obj[key]) === "object"){
+							freeze(obj[key], prefix + key + '.');
+						} else if(typeof(obj[key]) === "string"){
+							console.log('expects(' + prefix + key + ').toBe("' + obj[key] + '")');
+						} else if(typeof(obj[key]) === "number"){
+							console.log('expects(' + prefix + key + ').toBe(' + obj[key] + ')');
+						}
+					}
 				}
 			}
 		}
